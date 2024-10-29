05d2a9fa401fcd9b4447525f4df58f4fb6c02be9
mmtg445 <mmtg445@gmail.com>
2024-10-29 21:12:58 +0600

    Update config.py

--- config.py
+++ config.py
@@ -63,9 +63,9 @@ class Config(object):
     
     # Url Shortner Information 
     TECH_VJ = bool(environ.get('TECH_VJ', True)) # Set False If you want shortlink off else True
-    TECH_VJ_URL = environ.get('TECH_VJ_URL', 'moneykamalo.com') # your shortlink url domain or url without https://
-    TECH_VJ_API = environ.get('TECH_VJ_API', '0eefb93e1e3ce9470a7033115ceb1bad13a9d674') # your url shortner api
-    TECH_VJ_TUTORIAL = os.environ.get("TECH_VJ_TUTORIAL", "https://t.me/How_To_Open_Linkl")
+    TECH_VJ_URL = environ.get('TECH_VJ_URL', '') # your shortlink url domain or url without https://
+    TECH_VJ_API = environ.get('TECH_VJ_API', '') # your url shortner api
+    TECH_VJ_TUTORIAL = os.environ.get("TECH_VJ_TUTORIAL", "")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
